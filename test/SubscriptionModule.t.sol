fb850da19c097e2d1a39c333b1204389b051e0e7
lumoswiz <lumoswiz@protonmail.com>
2025-06-04 22:25:19 +0200

    test: simple slice unit test

--- test/SubscriptionModule.t.sol
+++ test/SubscriptionModule.t.sol
@@ -2,9 +2,19 @@
 pragma solidity >=0.8.28 <0.9.0;
 
 import { Test } from "forge-std/Test.sol";
+import { CirclesLib } from "src/CirclesLib.sol";
 
 contract SubscriptionModuleTest is Test {
-    function testNothing() external pure {
-        assertEq(true, true);
+    using CirclesLib for bytes;
+
+    function test_slicey_boys(bytes calldata data) external {
+        vm.assume(data.length <= 64);
+        vm.assume(data.length >= 2);
+        uint256 start = data.length - 2;
+        uint256 end = data.length;
+        uint256 x1 = data.slice(start, end);
+
+        emit log_uint(x1);
+        emit log_bytes(data);
     }
 }
