28e0b084faf2b320f19c8423cebd4b54b75669ca
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-06-04 18:52:40 +0200

    chore: setup dependencies and remappings (#1)
    - Install dependencies
    - Use sensible `foundry.toml` settings
    - Add `remappings.txt`
    - Remove init contracts

---
+++ src/SubscriptionModule.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import { Module } from "@zodiac/contracts/core/Module.sol";
+
+contract SubscriptionModule is Module {
+    constructor(address _owner, address _avatar, address _target) {
+        bytes memory initParams = abi.encode(_owner, _avatar, _target);
+        setUp(initParams);
+    }
+
+    function setUp(bytes memory initParams) public override initializer {
+        (address _owner, address _avatar, address _target) = abi.decode(initParams, (address, address, address));
+
+        __Ownable_init(msg.sender);
+
+        setAvatar(_avatar);
+        setTarget(_target);
+
+        transferOwnership(_owner);
+    }
+}
---
+++ test/SubscriptionModule.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.28 <0.9.0;
+
+import { Test } from "forge-std/Test.sol";
+
+contract SubscriptionModuleTest is Test {
+    function testNothing() external pure {
+        assertEq(true, true);
+    }
+}
