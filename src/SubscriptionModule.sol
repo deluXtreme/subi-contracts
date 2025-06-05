0325bca04bf05dfc4ba5c6f70aa31f1b14144a1a
lumoswiz <lumoswiz@protonmail.com>
2025-06-05 15:49:22 +0200

    feat: initial module setup with revoked nonces

--- src/SubscriptionModule.sol
+++ src/SubscriptionModule.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract SubscriptionModule { }
+import { RevokedNonce } from "src/RevokedNonce.sol";
+
+contract SubscriptionModule is RevokedNonce {
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    string public constant NAME = "Subscription Module";
+    string public constant VERSION = "0.0.1";
+}
