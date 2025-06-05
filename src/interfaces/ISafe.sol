b707276ebcc341be02e2a6512ea1cb345f35075e
lumoswiz <lumoswiz@protonmail.com>
2025-06-05 15:54:58 +0200

    feat: safe interface

---
+++ src/interfaces/ISafe.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+import { Enum } from "@safe-smart-account/contracts/common/Enum.sol";
+
+interface ISafe {
+    /// @dev Allows a Module to execute a Safe transaction without any further confirmations.
+    /// @param to Destination address of module transaction.
+    /// @param value Ether value of module transaction.
+    /// @param data Data payload of module transaction.
+    /// @param operation Operation type of module transaction.
+    function execTransactionFromModule(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation
+    )
+        external
+        returns (bool success);
+}
