38f74fc96335a1d77459bdefca0e36a01ae4a54d
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-06-11 15:50:08 +0200

    test: scaffold test suite with utils (#9)

---
+++ test/harness/ModuleHarness.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.28 <0.9.0;
+
+import { SubscriptionModule } from "src/SubscriptionModule.sol";
+import { Subscription } from "src/libs/Types.sol";
+
+contract ModuleHarness is SubscriptionModule {
+    function exposed__subscribe(address subscriber, bytes32 id, Subscription memory subscription) external {
+        _subscribe(subscriber, id, subscription);
+    }
+
+    function exposed__unsubscribe(address subscriber, bytes32 id) external {
+        _unsubscribe(subscriber, id);
+    }
+
+    function exposed__exists(bytes32 id) external view returns (bool) {
+        return _exists(id);
+    }
+
+    function exposed__loadSubscription(bytes32 id) external view returns (address safe, Subscription memory sub) {
+        (safe, sub) = _loadSubscription(id);
+    }
+
+    function exposed__requireRedeemablePeriods(Subscription memory sub) external view returns (uint256 periods) {
+        periods = _requireRedeemablePeriods(sub);
+    }
+
+    function exposed__applyRedemption(address safe, bytes32 id, Subscription memory sub, uint256 periods) external {
+        _applyRedemption(safe, id, sub, periods);
+    }
+}
---
+++ test/utils/Modifiers.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.22;
+
+import { Constants } from "./Constants.sol";
+import { Defaults } from "./Defaults.sol";
+import { Users } from "./Types.sol";
+import { Utils } from "./Utils.sol";
+
+abstract contract Modifiers is Constants, Utils {
+    /*//////////////////////////////////////////////////////////////
+                               VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    Defaults private defaults;
+    Users private users;
+
+    /*//////////////////////////////////////////////////////////////
+                                HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function setVariables(Defaults _defaults, Users memory _users) public {
+        defaults = _defaults;
+        users = _users;
+    }
+}
