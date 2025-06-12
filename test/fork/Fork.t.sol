9acfeca9366eb138a6c95c4eb7407d4914fda52c
lumoswiz <lumoswiz@protonmail.com>
2025-06-12 16:09:10 +0200

    test: basic fork testing

--- test/fork/Fork.t.sol
+++ test/fork/Fork.t.sol
@@ -2,16 +2,143 @@
 pragma solidity >=0.8.28 <0.9.0;
 
 import { SubscriptionModule } from "src/SubscriptionModule.sol";
+import { ISafe } from "src/interfaces/ISafe.sol";
 
 import { Assertions } from "../utils/Assertions.sol";
 import { Utils } from "../utils/Utils.sol";
 
+import { IHubV2 } from "@circles/src/hub/IHub.sol";
+import { TypeDefinitions } from "@circles/src/hub/TypeDefinitions.sol";
+import { Enum } from "@safe-smart-account/contracts/common/Enum.sol";
+import { ModuleManager } from "@safe-smart-account/contracts/base/ModuleManager.sol";
+
 abstract contract Fork_Test is Assertions, Utils {
     SubscriptionModule internal module;
+
+    uint256 internal pk = vm.envUint("PRIVATE_KEY");
+
+    address internal constant FROM = 0xeDe0C2E70E8e2d54609c1BdF79595506B6F623FE;
+
+    IHubV2 internal constant hub = IHubV2(0xc12C1E50ABB450d6205Ea2C3Fa861b3B834d13e8);
+
+    /*//////////////////////////////////////////////////////////////
+                       ALPHABETICAL-ORDER-STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
+    struct FlowInfo {
+        Inputs inputs;
+        address to;
+        uint256 value;
+    }
+
+    struct Inputs {
+        FlowEdge[] flow;
+        address[] flowVertices;
+        bytes packedCoordinates;
+        uint256 sourceCoordinate;
+        Stream[] streams;
+    }
+
+    struct FlowEdge {
+        uint192 amount;
+        uint16 streamSinkId;
+    }
+
+    struct Stream {
+        bytes data;
+        uint16[] flowEdgeIds;
+        uint16 sourceCoordinate;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 SET-UP
+    //////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual {
         vm.createSelectFork({ blockNumber: 40_531_966, urlOrAlias: "gnosis" });
 
         module = new SubscriptionModule();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function _toTypeEdge(FlowEdge memory e) internal pure returns (TypeDefinitions.FlowEdge memory) {
+        return TypeDefinitions.FlowEdge({ streamSinkId: e.streamSinkId, amount: e.amount });
+    }
+
+    function _toTypeStream(Stream memory s) internal pure returns (TypeDefinitions.Stream memory) {
+        return
+            TypeDefinitions.Stream({ sourceCoordinate: s.sourceCoordinate, flowEdgeIds: s.flowEdgeIds, data: s.data });
+    }
+
+    function _toTypeInputs(Inputs memory in_)
+        internal
+        pure
+        returns (
+            TypeDefinitions.FlowEdge[] memory flowEdges,
+            address[] memory flowVertices,
+            bytes memory packedCoordinates,
+            uint256 sourceCoordinate,
+            TypeDefinitions.Stream[] memory streams
+        )
+    {
+        flowEdges = new TypeDefinitions.FlowEdge[](in_.flow.length);
+        for (uint256 i = 0; i < in_.flow.length; i++) {
+            flowEdges[i] = _toTypeEdge(in_.flow[i]);
+        }
+        streams = new TypeDefinitions.Stream[](in_.streams.length);
+        for (uint256 j = 0; j < in_.streams.length; j++) {
+            streams[j] = _toTypeStream(in_.streams[j]);
+        }
+        flowVertices = in_.flowVertices;
+        packedCoordinates = in_.packedCoordinates;
+        sourceCoordinate = in_.sourceCoordinate;
+    }
+
+    function _toTypeFlowInfo(FlowInfo memory info)
+        internal
+        pure
+        returns (
+            TypeDefinitions.FlowEdge[] memory flowEdges,
+            address[] memory flowVertices,
+            bytes memory packedCoordinates,
+            uint256 sourceCoordinate,
+            TypeDefinitions.Stream[] memory streams
+        )
+    {
+        (flowEdges, flowVertices, packedCoordinates, sourceCoordinate, streams) = _toTypeInputs(info.inputs);
+    }
+
+    function _enableModule() internal {
+        resetPrank({ msgSender: vm.addr(pk) });
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            pk,
+            ISafe(FROM).getTransactionHash(
+                FROM,
+                0,
+                abi.encodeCall(ModuleManager.enableModule, (address(module))),
+                Enum.Operation.Call,
+                0,
+                0,
+                0,
+                address(0),
+                payable(address(0)),
+                ISafe(FROM).nonce()
+            )
+        );
+        ISafe(FROM).execTransaction(
+            FROM,
+            0,
+            abi.encodeCall(ModuleManager.enableModule, (address(module))),
+            Enum.Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            payable(address(0)),
+            abi.encodePacked(r, s, v)
+        );
     }
 }
--- test/fork/Module.t.sol
+++ test/fork/Module.t.sol
@@ -4,116 +4,64 @@ pragma solidity >=0.8.28 <0.9.0;
 import { Fork_Test } from "./Fork.t.sol";
 
 import { stdJson } from "forge-std/StdJson.sol";
-import { Enum } from "@safe-smart-account/contracts/common/Enum.sol";
-import { ModuleManager } from "@safe-smart-account/contracts/base/ModuleManager.sol";
 import { TypeDefinitions } from "@circles/src/hub/TypeDefinitions.sol";
 
 contract Module_Fork_Test is Fork_Test {
     using stdJson for string;
 
-    struct Path {
-        address from;
-        Inputs inputs;
-        address to;
-        uint256 value;
-    }
-
-    struct Inputs {
-        FlowEdge[] flow;
-        address[] flowVertices;
-        bytes packedCoordinates;
-        uint256 sourceCoordinate;
-        Stream[] streams;
-    }
-
-    struct FlowEdge {
-        uint192 amount;
-        uint16 streamSinkId;
-    }
-
-    struct Stream {
-        bytes data;
-        uint16[] flowEdgeIds;
-        uint16 sourceCoordinate;
-    }
-
     string internal json;
-
-    uint256 internal pk;
 
     function setUp() public override {
         Fork_Test.setUp();
 
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/test/data/inputs.json");
-        json = vm.readFile(path);
-
-        pk = vm.envUint("PRIVATE_KEY");
+        json = vm.readFile(string.concat(vm.projectRoot(), "/test/data/inputs.json"));
     }
 
-    function test_Scenario_0_manual() external {
-        //        bytes memory rawBlob = json.parseRaw(".3");
-        //        Path memory path = abi.decode(rawBlob, (Path));
-        //
-        //        bytes memory txData = abi.encodeCall(ModuleManager.enableModule, (address(module)));
-        //
-        //        resetPrank({ msgSender: vm.addr(pk) });
-        //
-        //        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-        //            pk,
-        //            ISafe(path.from).getTransactionHash(
-        //                path.from,
-        //                0,
-        //                txData,
-        //                Enum.Operation.Call,
-        //                0,
-        //                0,
-        //                0,
-        //                address(0),
-        //                payable(address(0)),
-        //                ISafe(path.from).nonce()
-        //            )
-        //        );
-        //        bytes memory signature = abi.encodePacked(r, s, v);
-        //
-        //        ISafe(path.from).execTransaction(
-        //            path.from, 0, txData, Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), signature
-        //        );
-        //
-        //        resetPrank({ msgSender: path.from });
-        //        bytes32 id = module.subscribe(path.to, path.value, 3600, true);
-        //
-        //        TypeDefinitions.FlowEdge[] memory flowEdges = new TypeDefinitions.FlowEdge[](path.inputs.flow.length);
-        //        for (uint256 i = 0; i < path.inputs.flow.length; i++) {
-        //            // note ordering in TypeDefinitions.FlowEdge is (streamSinkId, amount)
-        //            flowEdges[i] = TypeDefinitions.FlowEdge({
-        //                streamSinkId: path.inputs.flow[i].streamSinkId,
-        //                amount: path.inputs.flow[i].amount
-        //            });
-        //        }
-        //
-        //        // allocate and populate Stream[]
-        //        TypeDefinitions.Stream[] memory streams = new TypeDefinitions.Stream[](path.inputs.streams.length);
-        //        for (uint256 i = 0; i < path.inputs.streams.length; i++) {
-        //            // ordering in TypeDefinitions.Stream is (sourceCoordinate, flowEdgeIds, data)
-        //            streams[i] = TypeDefinitions.Stream({
-        //                sourceCoordinate: path.inputs.streams[i].sourceCoordinate,
-        //                flowEdgeIds: path.inputs.streams[i].flowEdgeIds, // dynamic array copies fine
-        //                data: path.inputs.streams[i].data
-        //            });
-        //        }
-        //
-        //        resetPrank({ msgSender: path.to });
-        //        module.redeem(
-        //            id,
-        //            path.inputs.flowVertices,
-        //            flowEdges,
-        //            streams,
-        //            path.inputs.packedCoordinates,
-        //            path.inputs.sourceCoordinate
-        //        );
-        //    }
+    function test_Scenario_0() external {
+        bytes memory rawBlob = json.parseRaw(".0");
+        FlowInfo memory info = abi.decode(rawBlob, (FlowInfo));
+
+        _enableModule();
+
+        (
+            TypeDefinitions.FlowEdge[] memory flowEdges,
+            address[] memory flowVertices,
+            bytes memory packedCoordinates,
+            uint256 sourceCoordinate,
+            TypeDefinitions.Stream[] memory streams
+        ) = _toTypeFlowInfo(info);
+
+        resetPrank({ msgSender: FROM });
+        bytes32 id = module.subscribe(info.to, info.value, 3600, true);
+
+        resetPrank({ msgSender: info.to });
+        module.redeem(
+            id,
+            info.inputs.flowVertices,
+            flowEdges,
+            streams,
+            info.inputs.packedCoordinates,
+            info.inputs.sourceCoordinate
+        );
+    }
+
+    /// @dev Trusted path fails for this scenario, passes with untrusted flow
+    function test_Scenario_1() external {
+        bytes memory rawBlob = json.parseRaw(".1");
+        FlowInfo memory info = abi.decode(rawBlob, (FlowInfo));
+
+        _enableModule();
+
+        resetPrank({ msgSender: FROM });
+        bytes32 id = module.subscribe(info.to, info.value, 3600, false);
+
+        uint256 cachedToBal = hub.balanceOf(info.to, uint256(uint160(FROM)));
+        uint256 cachedFromBal = hub.balanceOf(FROM, uint256(uint160(FROM)));
+
+        resetPrank({ msgSender: info.to });
+        module.redeemUntrusted(id);
+
+        assertEq(hub.balanceOf(info.to, uint256(uint160(FROM))), cachedToBal + info.value);
+        assertEq(hub.balanceOf(FROM, uint256(uint160(FROM))), cachedFromBal - info.value);
     }
 }
-
-interface ISafe { }
